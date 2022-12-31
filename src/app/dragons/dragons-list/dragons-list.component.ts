cb1926f1cc56cbc479a385c5a9f49f1b0eb42c50
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2022-12-31 18:00:59 -0300

    dragons-list works!

--- src/app/dragons/dragons-list/dragons-list.component.ts
+++ src/app/dragons/dragons-list/dragons-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DragonService } from '../services/dragon.service';
 
 @Component({
   selector: 'app-dragons-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dragons-list.component.scss']
 })
 export class DragonsListComponent {
+  dragons$ = this.dragonService.getAll();
 
+  constructor(private readonly dragonService: DragonService) { }
 }
