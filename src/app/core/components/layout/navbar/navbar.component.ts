8246b3f2f1a857f932a0173333dc2b52afd4fc1d
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-01-01 13:01:55 -0300

    layout improvements; home & about pages

---
+++ src/app/core/components/layout/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Link } from 'src/app/core/interfaces';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  links: Link[] = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'Dragons',
+      link: 'dragons',
+    },
+    {
+      name: 'Register',
+      link: 'dragons/create',
+    },
+    {
+      name: 'About',
+      link: 'about',
+    },
+  ];
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,30 +2,52 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    LayoutComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {
         path: '',
-        pathMatch: 'full',
-        redirectTo: 'dragons',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            component: HomepageComponent,
+            title: 'Home'
+          },
+          {
+            path: 'dragons',
+            loadChildren: () => import('../dragons/dragons.module').then(m => m.DragonsModule),
+            title: 'Dragons',
+          },
+          {
+            path: 'about',
+            component: AboutComponent,
+            title: 'About',
+          },
+          {
+            path: '**',
+            component: NotFoundComponent,
+            title: 'Not found',
+          },
+        ]
       },
-      {
-        path: 'dragons',
-        loadChildren: () => import('../dragons/dragons.module').then(m => m.DragonsModule),
-        title: 'Dragons',
-      },
-      {
-        path: '**',
-        component: NotFoundComponent,
-      }
     ]),
   ]
 })
